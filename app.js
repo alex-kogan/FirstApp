9b5f48692f18c9e37d447bfdd8dc7e06ffdd22df
alex-kogan <alx.kogan@yahoo.com>
2016-09-08 20:32:11 +0300

    change project struct
    moved to a simpler project structure, the tech is a bit more outdated
    but easier to handle at this point

--- app.js
+++ app.js
@@ -1,60 +1,23 @@
+'use strict';
+
+// Import our express and our configuration
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var configure = require('./config.js');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// Import our controllers
+var indexControllers = require('./controllers/index.js');
 
+// Create our express app
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Configure it
+configure(app);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Add routes mapping URLs to controllers
+app.get('/', indexControllers.index);
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
